2e0346587799b8e887480c8a274ef0d4720bf019
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2020-11-11 12:42:05 +0100

    Added check for response
    Added check for response, only perform action if result count equals 1 singular object.

--- create.ps1
+++ create.ps1
@@ -28,70 +28,72 @@ try{
 
     $getUri = $BaseUri + "/connectors/" + $getConnector + "?filterfieldids=Persoonsnummer&filtervalues=$personId"
     $getResponse = Invoke-RestMethod -Method Get -Uri $getUri -ContentType "application/json;charset=utf-8" -Headers $Headers -UseBasicParsing
+    
+    if($getResponse.rows.Count -eq 1){
+        #Change mapping here
+        # If 'EmailPortal' matches 'Email_werk_gebruiker', skip 'EmailPortal' in update body. AFAS will throw an error when trying to update this with the same value
+        if($getResponse.rows.Email_werk_gebruiker -eq $userPrincipalName){
+            # Update without 'EmailPortal'
+            $account = [PSCustomObject]@{
+                'KnPerson' = @{
+                    'Element' = @{
+                        'Fields' = @{
+                            # Zoek op BcCo (Persoons-ID)
+                            'MatchPer' = "0";
+                            # Nummer
+                            'BcCo' = $getResponse.rows.Persoonsnummer;
 
-    #Change mapping here
-    # If 'EmailPortal' matches 'Email_werk_gebruiker', skip 'EmailPortal' in update body. AFAS will throw an error when trying to update this with the same value
-    if($getResponse.rows.Email_werk_gebruiker -eq $userPrincipalName){
-        # Update without 'EmailPortal'
-        $account = [PSCustomObject]@{
-            'KnPerson' = @{
-                'Element' = @{
-                    'Fields' = @{
-                        # Zoek op BcCo (Persoons-ID)
-                        'MatchPer' = "0";
-                        # Nummer
-                        'BcCo' = $getResponse.rows.Persoonsnummer;
-            
-                        # E-Mail werk  
-                        'EmAd' = $emailaddress;
-                                
-                        <#
-                        # phone.business.fixed
-                        'TeNr' = $telephoneNumber;
-                        # phone.business.mobile
-                        'MbNr' = $mobile;
-                        #>
+                            # E-Mail werk  
+                            'EmAd' = $emailaddress;
+
+                            <#
+                            # phone.business.fixed
+                            'TeNr' = $telephoneNumber;
+                            # phone.business.mobile
+                            'MbNr' = $mobile;
+                            #>
+                        }
+                    }
+                }
+            }
+        }else{
+            # Update with 'EmailPortal'
+            $account = [PSCustomObject]@{
+                'KnPerson' = @{
+                    'Element' = @{
+                        'Fields' = @{
+                            # Zoek op BcCo (Persoons-ID)
+                            'MatchPer' = "0";
+                            # Nummer
+                            'BcCo' = $getResponse.rows.Persoonsnummer;
+
+                            # E-Mail werk  
+                            'EmAd' = $emailaddress;
+                            # E-Mail toegang
+                            'EmailPortal' = $userPrincipalName;
+
+                            <#
+                            # phone.business.fixed
+                            'TeNr' = $telephoneNumber;
+                            # phone.business.mobile
+                            'MbNr' = $mobile;
+                            #>
+                        }
                     }
                 }
             }
         }
-    }else{
-        # Update with 'EmailPortal'
-        $account = [PSCustomObject]@{
-            'KnPerson' = @{
-                'Element' = @{
-                    'Fields' = @{
-                        # Zoek op BcCo (Persoons-ID)
-                        'MatchPer' = "0";
-                        # Nummer
-                        'BcCo' = $getResponse.rows.Persoonsnummer;
-            
-                        # E-Mail werk  
-                        'EmAd' = $emailaddress;
-                        # E-Mail toegang
-                        'EmailPortal' = $userPrincipalName;
-                                
-                        <#
-                        # phone.business.fixed
-                        'TeNr' = $telephoneNumber;
-                        # phone.business.mobile
-                        'MbNr' = $mobile;
-                        #>
-                    }
-                }
-            }
+
+        if(-Not($dryRun -eq $True)){
+            $body = $account | ConvertTo-Json -Depth 10
+            $putUri = $BaseUri + "/connectors/" + $updateConnector
+
+            $putResponse = Invoke-RestMethod -Method Put -Uri $putUri -Body $body -ContentType "application/json;charset=utf-8" -Headers $Headers -UseBasicParsing
+            $aRef = $($account.KnPerson.Element.Values.BcCo)
         }
+        $success = $True;
+        $auditMessage = " $($account.KnPerson.Element.Values.BcCo) already exists. Identity updated instead of";
     }
-
-    if(-Not($dryRun -eq $True)){
-        $body = $account | ConvertTo-Json -Depth 10
-        $putUri = $BaseUri + "/connectors/" + $updateConnector
-
-        $putResponse = Invoke-RestMethod -Method Put -Uri $putUri -Body $body -ContentType "application/json;charset=utf-8" -Headers $Headers -UseBasicParsing
-        $aRef = $($account.KnPerson.Element.Values.BcCo)
-    }
-    $success = $True;
-    $auditMessage = " $($account.KnPerson.Element.Values.BcCo) already exists. Identity updated instead of"; 
 }catch{
     $errResponse = $_;
     $auditMessage = " $($account.KnPerson.Element.Values.BcCo) : ${errResponse}";
