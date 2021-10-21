98ba48a474bff90549922a6f5ee7a88c1cf5fc60
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2021-10-21 16:06:58 +0200

    Updated to use aref object and write-Warning

--- delete.ps1
+++ delete.ps1
@@ -16,9 +16,10 @@ $auditLogs = New-Object Collections.Generic.List[PSCustomObject];
 # Set TLS to accept TLS, TLS 1.1 and TLS 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
 
-$personId = $p.externalId; # Profit Employee Medewerker
-$emailaddress = "$personId@domain.com"; # Unique value based of PersonId because at the revoke action we want to clear the unique fields
-$userPrincipalName = "$personId@domain.com"; # Unique value based of PersonId because at the revoke action we want to clear the unique fields
+$filterfieldid = "Persoonsnummer"
+$filtervalue = $aRef.Persoonsnummer; # Has to match the AFAS value of the specified filter field ($filterfieldid)
+$emailaddress = "$($aRef.Persoonsnummer)@domain.com"; # Unique value based of PersonId because at the revoke action we want to clear the unique fields
+$userPrincipalName = "$($aRef.Persoonsnummer)@domain.com"; # Unique value based of PersonId because at the revoke action we want to clear the unique fields
 # $telephoneNumber = $p.Accounts.MicrosoftActiveDirectory.telephoneNumber;
 # $mobile = $p.Accounts.MicrosoftActiveDirectory.mobile;
 
@@ -26,9 +27,9 @@ try{
     $encodedToken = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($Token))
     $authValue = "AfasToken $encodedToken"
     $Headers = @{ Authorization = $authValue }
-    $getUri = $BaseUri + "/connectors/" + $getConnector + "?filterfieldids=Persoonsnummer&filtervalues=$personId&operatortypes=1"
+    $getUri = $BaseUri + "/connectors/" + $getConnector + "?filterfieldids=$filterfieldid&filtervalues=$filtervalue&operatortypes=1"
     $getResponse = Invoke-RestMethod -Method Get -Uri $getUri -ContentType "application/json;charset=utf-8" -Headers $Headers -UseBasicParsing
-    
+
     if($getResponse.rows.Count -eq 1){
         # Retrieve current account data for properties to be updated
         $previousAccount = [PSCustomObject]@{
@@ -68,8 +69,8 @@ try{
                                     # Nummer
                                     'BcCo' = $getResponse.rows.Persoonsnummer;
 
-                                    # E-Mail toegang
-                                    'EmailPortal' = $userPrincipalName;
+                                    # E-Mail toegang - Check with AFAS Administrator if this needs to be set
+                                    # 'EmailPortal' = $userPrincipalName;
 
                                     <#
                                     # phone.business.fixed
@@ -83,15 +84,15 @@ try{
                     })
                 }
             }
-        }
+        }     
 
         # If '$emailAdddres' does not match current 'EmAd', add 'EmAd' to update body. AFAS will throw an error when trying to update this with the same value
-        if($getResponse.rows.Email_werk -ne $emailaddress){
+        if( $getResponse.rows.Email_werk -ne $emailaddress ){
             # E-mail werk
             $account.'AfasEmployee'.'Element'.Objects[0].'KnPerson'.'Element'.'Fields' += @{'EmAd' = $emailaddress}
             Write-Verbose -Verbose "Updating BusinessEmailAddress '$($getResponse.rows.Email_werk)' with new value '$emailaddress'"
         }   
-        
+
         if(-Not($dryRun -eq $True)){
             $body = $account | ConvertTo-Json -Depth 10
 
@@ -101,7 +102,7 @@ try{
 
         $auditLogs.Add([PSCustomObject]@{
             Action = "DeleteAccount"
-            Message = "Deleted link and updated fields of account with id $aRef"
+            Message = "Deleted link and updated fields of account with id $($aRef.Medewerker)"
             IsError = $false;
         });
 
@@ -110,10 +111,10 @@ try{
 }catch{
     $auditLogs.Add([PSCustomObject]@{
         Action = "DeleteAccount"
-        Message = "Error deleting link and updating fields of account with Id $($aRef): $($_)"
+        Message = "Error deleting link and updating fields of account with Id $($aRef.Medewerker): $($_)"
         IsError = $True
     });
-    Write-Error $_;
+    Write-Warning $_;
 }
 
 # Send results
@@ -122,13 +123,5 @@ $result = [PSCustomObject]@{
 	AccountReference= $aRef;
 	AuditLogs = $auditLogs;
     Account = $account;
-    PreviousAccount = $previousAccount;    
-
-    # Optionally return data for use in other systems
-    ExportData       = [PSCustomObject]@{
-        EmployeeId              = $($account.AfasEmployee.Values.'@EmId')
-        BusinessEmailAddress    = $($account.AfasEmployee.Element.Objects[0].KnPerson.Element.Fields.EmAd)
-        PortalEmailAddress      = $($account.AfasEmployee.Element.Objects[0].KnPerson.Element.Fields.EmailPortal)
-    };    
 };
 Write-Output $result | ConvertTo-Json -Depth 10;
